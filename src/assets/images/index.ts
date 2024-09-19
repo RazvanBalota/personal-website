613ab2394d3971af4a690494710d37a26d1e2c52
razvan <razvan@whitecitycode.com>
2024-09-19 14:49:08 +0300

    feat: hero section

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as avatar } from "./avatar.webp"
+export { default as palms } from "./palms.avif"
+export { default as macbook } from "./macbook.avif"
+export { default as waves } from "./waves.avif"
+export { default as sky } from "./sky.jpg"
+export { default as robot } from "./robot.jpg"
