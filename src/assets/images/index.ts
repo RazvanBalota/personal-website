c2bb987495685cad25a48309518dcfea40115d84
Razvan <razvan@whitecitycode.com>
2024-09-19 18:30:38 +0300

    feat: employees card

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,9 @@
-export { default as avatar } from "./avatar.webp"
-export { default as palms } from "./palms.avif"
-export { default as macbook } from "./macbook.avif"
-export { default as waves } from "./waves.avif"
-export { default as sky } from "./sky.jpg"
-export { default as robot } from "./robot.jpg"
+export { default as avatar } from "./avatar.webp";
+export { default as palms } from "./palms.avif";
+export { default as macbook } from "./macbook.avif";
+export { default as waves } from "./waves.avif";
+export { default as sky } from "./sky.jpg";
+export { default as robot } from "./robot.jpg";
+export { default as wcc } from "./wcc.png";
+export { default as utest } from "./utest.png";
+export { default as letter_f } from "./letter_f.png";
