bba6d3d347136199bf230ea94be7267cf76aa27e
Razvan <razvan@whitecitycode.com>
2024-09-21 11:21:34 +0300

    feat: projects

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -8,3 +8,10 @@ export { default as wcc } from "./wcc.png";
 export { default as utest } from "./utest.png";
 export { default as letter_f } from "./letter_f.png";
 export { default as portraitImage } from "./portraitImage.jpg";
+
+
+export { default as logoAltnet } from "./logoAltnet.svg";
+export { default as logoAI3 } from "./logoAI3.svg";
+export { default as logoDifffusion } from "./logoDifffusion.png";
+export { default as logoCoderDojo } from "./logoCoderDojo.png";
+export { default as logoMelculzen } from "./logoMelculzen.svg";
