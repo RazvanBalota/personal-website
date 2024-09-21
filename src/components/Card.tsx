54f32b7da34fc9ce7ef21bec043dbce2b182d696
Razvan <razvan@whitecitycode.com>
2024-09-21 11:29:27 +0300

    fix: rm theme classes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -27,7 +27,7 @@ Card.Link = function CardLink({
 }: React.HTMLProps<HTMLAnchorElement>) {
   return (
     <>
-      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
+      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50" />
       <a {...props}>
         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
@@ -47,7 +47,7 @@ Card.Title = function CardTitle<T extends React.ElementType = "h2">({
   let Component = as ?? "h2";
 
   return (
-    <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+    <Component className="text-base font-semibold tracking-tight text-zinc-100">
       {href ? <Card.Link href={href}>{children}</Card.Link> : children}
     </Component>
   );
@@ -59,7 +59,7 @@ Card.Description = function CardDescription({
   children: React.ReactNode;
 }) {
   return (
-    <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+    <p className="relative z-10 mt-2 text-sm text-zinc-400">
       {children}
     </p>
   );
@@ -93,7 +93,7 @@ Card.Eyebrow = function CardEyebrow<T extends React.ElementType = "p">({
     <Component
       className={clsx(
         className,
-        "relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500",
+        "relative z-10 order-first mb-3 flex items-center text-sm text-zinc-500",
         decorate && "pl-3.5"
       )}
       {...props}
@@ -103,7 +103,7 @@ Card.Eyebrow = function CardEyebrow<T extends React.ElementType = "p">({
           className="absolute inset-y-0 left-0 flex items-center"
           aria-hidden="true"
         >
-          <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+          <span className="h-4 w-0.5 rounded-full bg-zinc-500" />
         </span>
       )}
       {children}
